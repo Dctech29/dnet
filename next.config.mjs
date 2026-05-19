fcb50751102219fe04b202638e980ce610b220e2
Deepak <deepak@Deepaks-MacBook-Air.local>
2026-05-19 10:51:16 +0530

    databse integrated

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,14 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'picsum.photos',
             },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'randomuser.me',
+            },
         ],
     },
 };
